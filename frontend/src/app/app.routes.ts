b0d3e885ded67cc699d529e331863483668ef294
Raphael Pattie <rpattiecsi@gmail.com>
2025-03-24 19:53:30 +0100

    feat(ai-11): tasks works

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CalculatorComponent } from "./components/calculator/calculator.component";
+import { TaskComponent } from "./components/task/task.component";
 import { WelcomeComponent } from "./components/welcome/welcome.component";
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
 	{ path: 'welcome', component: WelcomeComponent },
-	{ path: 'calculator', component: CalculatorComponent }
+	{ path: 'calculator', component: CalculatorComponent },
+	{ path: 'tasks', component: TaskComponent }
 ];
