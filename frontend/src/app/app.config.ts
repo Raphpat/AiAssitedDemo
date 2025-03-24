c60cc8b73bffabd125ca4727138cd95e8d2e7a75
Raphael Pattie <rpattiecsi@gmail.com>
2025-03-24 19:44:10 +0100

    reset again

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,16 +2,11 @@ import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideStore } from "@ngrx/store";
 import { routes } from './app.routes';
-import { taskReducer } from "./store/task/task.reducer";
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
-		provideHttpClient(),
-		provideStore({
-      tasks: taskReducer
-    }),
+		provideHttpClient()
 	]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { CalculatorComponent } from "./components/calculator/calculator.component";
-import { TaskComponent } from "./components/task/task.component";
 import { WelcomeComponent } from "./components/welcome/welcome.component";
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
 	{ path: 'welcome', component: WelcomeComponent },
-	{ path: 'calculator', component: CalculatorComponent },
-	{ path: 'tasks', component: TaskComponent }
+	{ path: 'calculator', component: CalculatorComponent }
 ];
